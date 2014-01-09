3adbb3973a7226ef39a0302e5e9a2269892578f8
Calin-Lucian <calin@the4thfloor.ro>
2014-01-09 22:47:31 +0200

    this one

---
+++ function.php
@@ -0,0 +1,25 @@
+function custom_field_excerpt() {
+    
+    global $post;
+    $text = apply_filters( 'the_content', $post->post_content );
+    //var_dump( $text );
+    if ( '' != $text ) {
+		$text = preg_replace( '/(<img)(.*?)(src=")(.*?)(.jpg")(.*?)>?/i', '', $text );
+		
+        $start = strpos( $text, '<p' ); // Locate the first paragraph tag
+        
+        $start_1 = strpos( $text, '</p>', $start ); // Locate the first paragraph closing tag
+        
+        $start_2 = strpos( $text, '<p', $start_1 ); // Locate the first paragraph tag
+        
+        $end = strpos($text, '</p>', $start_2); // Locate the first paragraph closing tag
+		
+		
+        $text = substr( $text, $start, $end-$start+3); // Trim off everything after the closing paragraph tag
+        $text = strip_shortcodes( $text );
+        
+        //$text = apply_filters( 'the_content', $text );
+
+    }
+    return $text;
+}
